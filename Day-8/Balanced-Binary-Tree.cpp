9572039edcc5c06f4ba8846d9aa777950ba224ad
Aman Jain <41456447+aman38649@users.noreply.github.com>
2023-01-15 16:20:45 +0530

    Add files via upload

---
+++ Day-8/Balanced-Binary-Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    
+private:
+    int treeDepth(TreeNode *node){
+        if (node == NULL)
+        {
+            return 0;
+        }
+        
+        int left = treeDepth(node->left);
+        int right = treeDepth(node->right);
+        
+        if (left == -1 || right == -1)
+        {
+            return -1;
+        }
+        
+        return  abs(left - right) > 1 ? -1 : max(left, right) + 1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        return treeDepth(root) != -1;
+    }
+};
